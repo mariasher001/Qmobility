f772c6858a5fd7d8ece56cffc8d5f5df99e1c0f9
mariasher001 <mariasher001@gmail.com>
2023-03-18 06:55:59 +0100

    Added Collecting BusinessLocation using GPS in RegisterBusinessActivity.java

--- app/src/main/java/com/mariasher/qmobilitybusiness/RegisterBusinessActivity.java
+++ app/src/main/java/com/mariasher/qmobilitybusiness/RegisterBusinessActivity.java
@@ -1,11 +1,22 @@
 package com.mariasher.qmobilitybusiness;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+
+import android.content.Context;
 import android.content.Intent;
+
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
@@ -16,11 +27,16 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.mariasher.qmobilitybusiness.database.BusinessInfo;
 import com.mariasher.qmobilitybusiness.databinding.ActivityRegisterBusinessBinding;
 
+import java.util.List;
 import java.util.UUID;
 
 public class RegisterBusinessActivity extends AppCompatActivity {
 
     private ActivityRegisterBusinessBinding binding;
+
+    private LocationManager locationManager;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 123;
+
     private FirebaseDatabase mReal;
 
 
@@ -50,7 +66,7 @@ public class RegisterBusinessActivity extends AppCompatActivity {
         String businessLongitude = binding.businessLongitudeEditText.getText().toString();
         String businessType = binding.businessTypeEditText.getText().toString();
         String locationManagersName = binding.locationManagerNameEditText.getText().toString();
-        String businessID = businessName+ "-" + UUID.randomUUID().toString();
+        String businessID = businessName + "-" + UUID.randomUUID().toString();
 
         if (checkBusinessInformation(businessName, businessPhoneNumber, businessEmail, businessAddress, businessLatitude,
                 businessLongitude, businessType, locationManagersName)) {
@@ -69,19 +85,19 @@ public class RegisterBusinessActivity extends AppCompatActivity {
             return setError(binding.businessNameEditText, " Business Name is Required!");
         if (businessPhoneNumber.isEmpty())
             return setError(binding.businessPhoneNumberEditText, "Business PhoneNumber is Required!");
-        if(businessEmail.isEmpty())
-            return setError(binding.businessEmailEditText,"BusinessEmail is Required!");
-        if(!Patterns.EMAIL_ADDRESS.matcher(businessEmail).matches())
-            return setError(binding.businessEmailEditText,"Please enter Correct Email Address");
+        if (businessEmail.isEmpty())
+            return setError(binding.businessEmailEditText, "BusinessEmail is Required!");
+        if (!Patterns.EMAIL_ADDRESS.matcher(businessEmail).matches())
+            return setError(binding.businessEmailEditText, "Please enter Correct Email Address");
         if (businessAddress.isEmpty())
             return setError(binding.businessAddressEditText, "BusinessAddress is Required!");
-        if(businessLatitude.isEmpty())
+        if (businessLatitude.isEmpty())
             return setError(binding.businessLatitudeEditText, "Business Latitude Coordinates are required!");
-        if(businessLongitude.isEmpty())
+        if (businessLongitude.isEmpty())
             return setError(binding.businessLongitudeEditText, "BusinessLongitude Coordinates are Required!");
-        if(businessType.isEmpty())
+        if (businessType.isEmpty())
             return setError(binding.businessTypeEditText, "BusinessType is Required!");
-        if(locationManagersName.isEmpty())
+        if (locationManagersName.isEmpty())
             return setError(binding.locationManagerNameEditText, "Location Manager Name is required!");
 
         return true;
@@ -113,5 +129,58 @@ public class RegisterBusinessActivity extends AppCompatActivity {
     }
 
     public void getLocationUsingGPSClicked(View view) {
+        if (binding.GetGpsBusinessAddressCheckBox.isChecked()) {
+            getGPSLocation();
+            binding.businessLatitudeEditText.setEnabled(false);
+            binding.businessLongitudeEditText.setEnabled(false);
+
+        } else {
+            binding.businessLatitudeEditText.setText("");
+            binding.businessLongitudeEditText.setText("");
+            binding.businessLatitudeEditText.setEnabled(true);
+            binding.businessLongitudeEditText.setEnabled(true);
+        }
     }
+
+    private void getGPSLocation() {
+        final LocationListener locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                Location mLocation = location;
+                Log.d("Location Coordinates", location.toString());
+                binding.businessLatitudeEditText.setText("" + location.getLatitude());
+                binding.businessLongitudeEditText.setText("" + location.getLongitude());
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+                Log.d("Provider Disabled", provider);
+                Toast.makeText(getApplicationContext(), "Please enable your location", Toast.LENGTH_LONG).show();
+            }
+        };
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Request the permission if it is not granted
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+        } else {
+            // Get the location manager
+            locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+
+            // Create a criteria with your requirements
+            Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_FINE);
+            criteria.setHorizontalAccuracy(Criteria.ACCURACY_HIGH);
+            criteria.setVerticalAccuracy(Criteria.ACCURACY_HIGH);
+
+            // Request location updates with the criteria and the location listener
+            locationManager.requestSingleUpdate(criteria, locationListener, null);
+        }
+    }
+
+
 }
+
+
