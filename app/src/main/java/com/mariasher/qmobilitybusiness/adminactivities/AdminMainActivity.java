558021ce8e21fb522bb476bed7c79b9e383faadd
Maria Sher <mariasher001@gmail.com>
2023-04-07 13:24:55 +0200

    Logout options menu added.

--- app/src/main/java/com/mariasher/qmobilitybusiness/adminactivities/AdminMainActivity.java
+++ app/src/main/java/com/mariasher/qmobilitybusiness/adminactivities/AdminMainActivity.java
@@ -2,12 +2,16 @@ package com.mariasher.qmobilitybusiness.adminactivities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.mariasher.qmobilitybusiness.LoginActivity;
+import com.mariasher.qmobilitybusiness.R;
 import com.mariasher.qmobilitybusiness.Utils.FirebaseRealtimeUtils;
 import com.mariasher.qmobilitybusiness.databinding.ActivityAdminMainBinding;
 
@@ -51,5 +55,17 @@ public class AdminMainActivity extends AppCompatActivity {
     public void viewAnalyticsButtonClicked(View view) {
 //        Intent intent = new Intent(this, ManageEmployeesActivity.class);
 //        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.logout_admin_options_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    public void logoutAdminItemClicked(@NonNull MenuItem item) {
+        mAuth.signOut();
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
