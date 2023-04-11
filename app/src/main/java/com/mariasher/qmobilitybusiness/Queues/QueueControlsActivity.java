ca3637fba4911d5d463dabccaeba8988a3ecf70e
Maria Sher <mariasher001@gmail.com>
2023-04-11 08:11:30 +0200

    QueueControls start, pause, reset and delete implemented.

--- app/src/main/java/com/mariasher/qmobilitybusiness/Queues/QueueControlsActivity.java
+++ app/src/main/java/com/mariasher/qmobilitybusiness/Queues/QueueControlsActivity.java
@@ -2,19 +2,26 @@ package com.mariasher.qmobilitybusiness.Queues;
 
 import static com.mariasher.qmobilitybusiness.Utils.Adapters.ViewQueuesViewAdapter.QUEUE_ID;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
+import com.mariasher.qmobilitybusiness.Utils.DateTimeUtils;
 import com.mariasher.qmobilitybusiness.Utils.FirebaseRealtimeUtils;
 import com.mariasher.qmobilitybusiness.Utils.enums.QueueStatus;
 import com.mariasher.qmobilitybusiness.database.Queue;
 import com.mariasher.qmobilitybusiness.databinding.ActivityQueueControlsBinding;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 
 public class QueueControlsActivity extends AppCompatActivity {
 
@@ -23,6 +30,8 @@ public class QueueControlsActivity extends AppCompatActivity {
     private FirebaseDatabase mReal;
     private FirebaseRealtimeUtils firebaseRealtimeUtils;
     private String queueId;
+    private String businessId;
+    private Queue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,7 +50,13 @@ public class QueueControlsActivity extends AppCompatActivity {
         mReal = FirebaseDatabase.getInstance();
         firebaseRealtimeUtils = new FirebaseRealtimeUtils(this);
 
-        firebaseRealtimeUtils.getQueueDataFromFirebaseDatabase(mAuth.getCurrentUser().getUid(), queueId, queue -> {
+        String Uid = mAuth.getCurrentUser().getUid();
+        firebaseRealtimeUtils.getBusinessIdFromEmployeeBusinessLink(Uid, businessId -> {
+            this.businessId = businessId;
+        });
+
+        firebaseRealtimeUtils.getQueueDataFromFirebaseDatabase(Uid, queueId, queue -> {
+            this.queue = queue;
             setViewValues(queue);
         });
     }
@@ -80,21 +95,39 @@ public class QueueControlsActivity extends AppCompatActivity {
     }
 
     public void startQueueControlsButtonClicked(View view) {
-        //TODO
+
+        if(queue.getQueueStatus().equals(QueueStatus.INACTIVE.toString())) {
+            queue.setQueueStartTime(DateTimeUtils.convertDateAndTimeToString(LocalDateTime.now()));
+        }
+        queue.setQueueEndTime("");
+
+        queue.setQueueStatus(QueueStatus.ACTIVE.toString());
+        updateQueueInFirebase("Queue Started");
     }
 
 
     public void pauseQueueControlsButtonClicked(View view) {
-        //TODO
+        queue.setQueueStatus(QueueStatus.PAUSED.toString());
+        updateQueueInFirebase("Queue Paused");
     }
 
     public void resetQueueControlsButtonClicked(View view) {
-        //TODO
+        queue.setQueueStatus(QueueStatus.INACTIVE.toString());
+        queue.setQueueEndTime(DateTimeUtils.convertDateAndTimeToString(LocalDateTime.now()));
+        updateQueueInFirebase("Queue Reset Successful");
     }
 
 
     public void deleteQueueControlsButtonClicked(View view) {
-        //TODO
+        new AlertDialog.Builder(this)
+                .setCancelable(true)
+                .setTitle("Are you sure you want to delete " + queue.getQueueName() + " queue?")
+                .setPositiveButton("YES", (dialog, i) -> {
+                    deleteQueueFromFirebase();
+                })
+                .setNegativeButton("NO", (dialog, i) -> {
+                    dialog.dismiss();
+                }).show();
     }
 
     public void queueDetailsControlsButtonClicked(View view) {
@@ -105,5 +138,49 @@ public class QueueControlsActivity extends AppCompatActivity {
 
     public void queueAnalyticsControlsButtonClicked(View view) {
         //TODO
+    }
+
+    private void updateQueueInFirebase(String queueToast) {
+        Map<String, Object> queueMap = new HashMap<>();
+        queueMap.put("queueId", queue.getQueueId());
+        queueMap.put("creatorId", queue.getCreatorId());
+        queueMap.put("queueName", queue.getQueueName());
+        queueMap.put("queueStartTime", queue.getQueueStartTime());
+        queueMap.put("queueEndTime", queue.getQueueEndTime());
+        queueMap.put("queueStatus", queue.getQueueStatus());
+        queueMap.put("numberOfActiveCounters", queue.getNumberOfActiveCounters());
+        queueMap.put("averageCustomerTime", queue.getAverageCustomerTime());
+
+        mReal.getReference("QMobility")
+                .child("Businesses")
+                .child(businessId)
+                .child("Queues")
+                .child(queueId)
+                .setValue(queueMap)
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(this, queueToast, Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(this, "Unable to update queue", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    private void deleteQueueFromFirebase() {
+        mReal.getReference("QMobility")
+                .child("Businesses")
+                .child(businessId)
+                .child("Queues")
+                .child(queueId)
+                .removeValue()
+                .addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        Toast.makeText(this, "Queue deleted successfully!", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(this, "Error deleting the queue!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        finish();
     }
 }
